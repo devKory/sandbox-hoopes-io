2c3d3d879c879174572c3d6311c0a7bafcffeff9
Kory Hoopes <kory@hey.com>
2022-10-11 16:08:58 -0600

    Pulling code from Replit.com
    Pulling code from the sandbox-hoopes-io Replit from Replit.com and putting it into this project.

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -1,1 +1,25 @@
-console.log("js/index.js");
+function tabs() {
+    document.querySelectorAll('.tabs-wrapper').forEach(tab => {
+        const items = tab.querySelectorAll('.tabs ul li');
+        const contents = tab.querySelectorAll('.tab-content');
+
+        items[0].classList.add('is-active');
+        contents[0].classList.add('is-active');
+
+        items.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                activateTab(index, items, contents);
+            });
+        });
+    });
+
+    function activateTab(index: number, items: any, contents: any) {
+        items.forEach((item: any) => item.classList.remove('is-active'));
+        items[index].classList.add('is-active');
+        
+        contents.forEach((content: any) => content.classList.remove('is-active'));
+        contents[index].classList.add('is-active');
+    }
+};
+
+tabs();
